5ae2c1df47d95d49d46ff9dfb20cf4d8ef39abb1
vikrant <mr.vikrantkumar121@gmail.com>
2021-08-21 02:12:20 +0530

    updated Cart and CartItem models adding foreign key from CartItem to Cart

--- cart/models.py
+++ cart/models.py
@@ -3,22 +3,23 @@ from store.models import Product
 
 # Create your models here.
 
+class Cart(models.Model):
+    """"""
+    cart_no = models.CharField(max_length = 250, blank = True)
+    # items = models.ManyToManyField(CartItem, on_delete = models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add = True)
+
+
+    def __str__(self):
+        return self.cart_no
+
 class CartItem(models.Model):
     """"""
     product = models.ForeignKey(Product, on_delete = models.CASCADE)
     quantity = models.IntegerField(default = 1)
+    cart = models.ForeignKey(Cart, on_delete = models.CASCADE, null = True)
     is_active = models.BooleanField(default = True)
     added_on = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
-        return self.product
-
-class Cart(models.Model):
-    """"""
-    cart_no = models.CharField(max_length = 250, blank = True)
-    items = models.ForeignKey(CartItem, on_delete = models.CASCADE)
-    created_on = models.DateTimeField(auto_now_add = True)
-    
-
-    def __str__(self):
-        return self.cart_no
+        return self.product.product_name
