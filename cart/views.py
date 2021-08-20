27fc6a0fb057478f946ad3c0c230a0a1751a8ff9
vikrant <mr.vikrantkumar121@gmail.com>
2021-08-20 22:02:47 +0530

    added cart_view

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Cart, CartItem
+from store.models import Product
 
 # Create your views here.
+
+def cart_view(request, cart_pk = None):
+    """"""
+    cart = None
+    cart_item = []
+    if cart_pk != None:
+        cart = get_object_or_404(Cart, pk = cart_pk)
+        cart_items = [get_object_or_404(CartItem, item) for item in cart.items]
+
+    context = {
+        'cart' : cart,
+        'cart_items': cart_items
+    }
+
+    return render(request,'store/cart.html')
