d5eddeb32719eba3c621255f7a02f0afbef448cc
vikrant <mr.vikrantkumar121@gmail.com>
2021-08-29 20:39:36 +0530

    added urls for the add cart and remove cart

--- cart/urls.py
+++ cart/urls.py
@@ -1,5 +1,10 @@
-form django.urls import path
+from django.urls import path
+from .views import cart_view, add_cart, remove_cart, remove_cart_all
 
 urlpatterns = [
-    
+    path('', cart_view, name = 'cart'),
+    path('add_cart/<int:product_pk>/', add_cart, name = 'add_cart'),
+    path('remove_cart/<int:product_pk>/', remove_cart, name = 'remove_cart'),
+    path('remove_cart_all/<int:product_pk>/', remove_cart_all, name = 'remove_cart_all'),
+
 ]
