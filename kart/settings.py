cf741c6a2c055d804439b48640db2b183e6895a3
vikrant <mr.vikrantkumar121@gmail.com>
2021-09-07 20:57:21 +0530

    change db config

--- kart/settings.py
+++ kart/settings.py
@@ -106,14 +106,14 @@ else:
 
             'ENGINE': 'django.db.backends.sqlite3',
 
-            'NAME': BASE_DIR/'db.sqlite3',            
+            'NAME': BASE_DIR/'db.sqlite3',
 
         }
 
     }
 
 
-
+LD_LIBRARY_PATH="/usr/local/lib"
 
 
 # Password validation
